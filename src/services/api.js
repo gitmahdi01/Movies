92b4ddfa735adb0f8ddcc6a893b5e3fa21ebd6eb
Mahdi Galant <111741778+gitmahdi01@users.noreply.github.com>
2026-06-27 10:02:00 +0200

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "55a038fb076ac2abdc189e28f61f9936";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
